f0b11daace34f0d1f0c3c6e6ed4c4d20a2b50a57
Ander Barbot <97862496+BarbotAnder@users.noreply.github.com>
2025-09-05 17:27:47 -0600

    footer change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,18 +4,17 @@ export default function Footer() {
   return (
     <footer className="footer sm:footer-horizontal footer-center bg-base-200 text-base-content p-4">
       <aside className="flex flex-col items-center space-y-2">
-        <p className="font-semibold">Ander Barbot</p>
-        <p>Phone: <a href="tel:2082302102" className="link">208-230-2102</a></p>
-        <p>Address: 3102 N Treasure Dr, Boise, ID 83703</p>
-        <p>Email: <a href="mailto:anderbarbot@gmail.com" className="link">anderbarbot@gmail.com</a></p>
+        <a href="tel:2082302102" className="link font-semibold">208-230-2102</a>
+        <p className="font-semibold">3102 N Treasure Dr, Boise, ID 83703</p>
+        <a href="mailto:anderbarbot@gmail.com" className="link font-semibold">anderbarbot@gmail.com</a>
         <div className="flex space-x-4 mt-2">
-          <Link href="https://www.facebook.com" className="link" target="_blank" aria-label="Facebook">
+          <Link href="https://www.facebook.com/ander.barbot/" className="link font-semibold" target="_blank" aria-label="Facebook">
             Facebook
           </Link>
-          <Link href="https://www.linkedin.com" className="link" target="_blank" aria-label="LinkedIn">
+          <Link href="https://www.https://www.linkedin.com/in/ander-barbot-025627183.com" className="link font-semibold" target="_blank" aria-label="LinkedIn">
             LinkedIn
           </Link>
-          <Link href="https://www.instagram.com" className="link" target="_blank" aria-label="Instagram">
+          <Link href="https://www.instagram.com" className="link font-semibold" target="_blank" aria-label="Instagram">
             Instagram
           </Link>
         </div>
